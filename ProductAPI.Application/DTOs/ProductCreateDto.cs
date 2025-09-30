e5bdb339bda74bc5de0b458c4c468c761b19dc93
Balraj Dhaliwal <balraj.d.nz@gmail.com>
2025-09-30 15:25:59 +1300

    Added validations to product dtos. update productconfiguration validations and created migrations #15

--- ProductAPI.Application/DTOs/ProductCreateDto.cs
+++ ProductAPI.Application/DTOs/ProductCreateDto.cs
@@ -1,9 +1,17 @@
-﻿namespace ProductAPI.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI.Application.DTOs
 {
     public class ProductCreateDto
     {
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(150)]
         public string Description { get; set; } = string.Empty;
+        [Required]
+        [Precision(7,2)]
         public decimal Price { get; set; }
     }
 }
