7194bfd012ed8868a6f9e06e29cd5d7c90bfa2b9
Balraj Dhaliwal <balraj.d.nz@gmail.com>
2025-09-30 14:08:45 +1300

    Added swagger examples for DTOs. Update Swashbuckle to latest. Decorate ProductController with SwaggerRequest and SwaggerResponse Examples #10

---
+++ ProductAPI/Swagger/Examples/ProductCreateDtoExample.cs
@@ -0,0 +1,24 @@
+﻿using ProductAPI.Application.DTOs;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace ProductAPI.Swagger.Examples
+{
+    /// <summary>
+    /// Example of what is sent when creating a new product.
+    /// </summary>
+    public class ProductCreateDtoExample : IExamplesProvider<ProductCreateDto>
+    {
+        /// <summary>
+        /// Example definition of object sent for product creation.
+        /// </summary>
+        public ProductCreateDto GetExamples()
+        {
+            return new ProductCreateDto
+            {
+                Name = "High-Performance Speakers",
+                Description = "The latest model with fantastic sound and bluetooth connectivity.",
+                Price = 299.99m
+            };
+        }
+    }
+}
---
+++ ProductAPI/Swagger/Examples/ProductUpdateDtoExample.cs
@@ -0,0 +1,24 @@
+﻿using ProductAPI.Application.DTOs;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace ProductAPI.Swagger.Examples
+{
+    /// <summary>
+    /// Example of what is sent when updating a product.
+    /// </summary>
+    public class ProductUpdateDtoExample : IExamplesProvider<ProductUpdateDto>
+    {
+        /// <summary>
+        /// Example definition of object sent for product update.
+        /// </summary>
+        public ProductUpdateDto GetExamples()
+        {
+            return new ProductUpdateDto
+            {
+                Name = "2026 Speakers",
+                Description = "New 2026 Model of Sony Speakers with amazing connectivity options and sound.",
+                Price = 399.99m
+            };
+        }
+    }
+}
