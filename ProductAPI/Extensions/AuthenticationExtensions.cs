fabadec8890a2eb8d7ab8c715d429f6fc93a5b0b
Balraj Dhaliwal <balraj.d.nz@gmail.com>
2026-01-22 16:05:49 +1300

    Implement Authorization using JWT Token with Auth0 as Identity Provider. Completed Setup of Swagger to allow Auth. Updated AppSettings with required Auth0 Fields to bind to Auth0Settings. Added new extension methods for swagger and auth to load in Program.cs. Added single permission in ProductController, Get method to test Auth0

--- ProductAPI/Extensions/AuthenticationExtensions.cs
+++ ProductAPI/Extensions/AuthenticationExtensions.cs
@@ -1,6 +1,44 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using ProductAPI.Infrastructure.Authentication;
+
 namespace ProductAPI.Extensions;
 
-public class AuthenticationExtensions
+public static class AuthenticationExtensions
 {
-    
+    public static IServiceCollection AddAuth0Authentication(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Bind and register Auth0 settings
+        var auth0Settings = configuration
+            .GetSection(Auth0Settings.SectionName)
+            .Get<Auth0Settings>();
+
+        if (auth0Settings == null)
+        {
+            throw new InvalidOperationException(
+                "Auth0 settings are not configured properly in appsettings.json");
+        }
+
+        services.Configure<Auth0Settings>(
+            configuration.GetSection(Auth0Settings.SectionName));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = $"https://{auth0Settings.Domain}/";
+                options.Audience = auth0Settings.Audience;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("ReadProduct", policy =>
+                policy.RequireClaim("permissions", "read:product"));
+        });
+        return services;
+    }
 }
