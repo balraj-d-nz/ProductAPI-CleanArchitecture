c82a402a935faa48214d6fdcf48e7e5c1815c5ed
Balraj Dhaliwal <balraj.d.nz@gmail.com>
2026-01-23 16:52:05 +1300

    #35 Implemented CurrentUserService.cs and added DI to Program.cs

--- ProductAPI.Infrastucture/Authentication/CurrentUserService.cs
+++ ProductAPI.Infrastucture/Authentication/CurrentUserService.cs
@@ -1,6 +1,23 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using ProductAPI.Application.Interfaces;
+
 namespace ProductAPI.Infrastructure.Authentication;
 
-public class CurrentUserService
+public class CurrentUserService : ICurrentUserService
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
     
+    public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+    public string? Email => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email);
+
+    public bool IsAuthenticated => _httpContextAccessor.HttpContext?.User?.Identity?.IsAuthenticated ?? false;
+
+    public IEnumerable<string> Permissions => _httpContextAccessor.HttpContext?.User?.FindAll("permissions").Select(p => p.Value) ?? Enumerable.Empty<string>();
 }
